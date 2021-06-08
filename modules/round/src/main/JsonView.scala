00762615847532e0269f190bbb232b26babd47c3
Matt Tucker <matt.watkinsmedia.org>
2021-06-08 14:14:46 +0100

    Remove loa debug logging

--- modules/round/src/main/JsonView.scala
+++ modules/round/src/main/JsonView.scala
@@ -51,7 +51,7 @@ final class JsonView(
       initialFen: Option[FEN],
       withFlags: WithFlags,
       nvui: Boolean
-  ): Fu[JsObject] = {
+  ): Fu[JsObject] =
     getSocketStatus(pov.game) zip
       (pov.opponent.userId ?? userRepo.byId) zip
       takebacker.isAllowedIn(pov.game) zip
@@ -124,7 +124,6 @@ final class JsonView(
             )
           })
       }
-  }
 
   private def commonWatcherJson(g: Game, p: GamePlayer, user: Option[User], withFlags: WithFlags): JsObject =
     Json
