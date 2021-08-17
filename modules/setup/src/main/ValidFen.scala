03e414666054f5c932b83403816b27e5ca352fcf
Lakin Wecker <lakin@structuredabstraction.com>
2021-08-16 21:39:54 -0600

    ValidFen has enough info to ascertain the lib. Use that.
    Otherwise it's not as usable in those other cases. Although, now that
    I'm looking at it, maybe this was undoing what you figured you needed to
    do.  However, if ValidFen is going to take a wrapped FEN, then it has
    enough information. If it wasn't going to take a wrapped FEN, then we
    need more than one of them (one for each type).
    
    My personal viewpoint is that ValidFen should take a wrapped one and the
    places that use it should appropriately do the wrapping.
    
    Looks like the places in app which were previously not compiling are now
    compiling, but are still hard coding the GameLib.Chess() values.

--- modules/setup/src/main/ValidFen.scala
+++ modules/setup/src/main/ValidFen.scala
@@ -10,10 +10,15 @@ case class ValidFen(fen: FEN, situation: strategygames.Situation) {
 
 object ValidFen {
 
-  def apply(strict: Boolean)(lib: GameLib, fen: FEN): Option[ValidFen] =
+  def apply(strict: Boolean)(fen: FEN): Option[ValidFen] = {
+    val lib = fen match {
+      case FEN.Chess(_) => GameLib.Chess()
+      case FEN.Draughts(_) => GameLib.Draughts()
+    }
     for {
       parsed <- strategygames.format.Forsyth.<<<(lib, fen)
       if parsed.situation playable strict
       validated = strategygames.format.Forsyth.>>(lib, parsed)
     } yield ValidFen(validated, parsed.situation)
+  }
 }
