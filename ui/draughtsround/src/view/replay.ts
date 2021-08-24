caa1dd2ea41de75b06f2cc718dadfd4184cfd984
Matt Tucker <matt.watkinsmedia.org>
2021-08-24 22:44:16 +0100

    Disable analysis for all draughts variants

--- ui/draughtsround/src/view/replay.ts
+++ ui/draughtsround/src/view/replay.ts
@@ -129,7 +129,7 @@ function renderMoves(ctrl: RoundController): MaybeVNodes {
 
 export function analysisButton(ctrl: RoundController): VNode | undefined {
   const forecastCount = ctrl.data.forecastCount;
-  return game.userAnalysable(ctrl.data)
+  return game.userAnalysable(ctrl.data) && util.allowAnalysisForVariant(ctrl.data.game.variant.key)
     ? h(
         'a.fbt.analysis',
         {
