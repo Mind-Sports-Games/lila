ecf854d15c8ad8f5b18189f70e98dd744c32a2a8
Matt Tucker <matt.watkinsmedia.org>
2021-11-16 16:58:15 +0000

    Make predrop work for shogi

--- ui/round/src/ground.ts
+++ ui/round/src/ground.ts
@@ -63,7 +63,7 @@ export function makeConfig(ctrl: RoundController): Config {
       },
     },
     predroppable: {
-      enabled: data.pref.enablePremove && data.game.variant.key === 'crazyhouse',
+      enabled: data.pref.enablePremove && (data.game.variant.key === 'crazyhouse' || data.game.variant.key === 'shogi'),
       events: {
         set: hooks.onPredrop,
         unset() {
