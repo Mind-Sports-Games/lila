0ec83c2683d69f2f9861fd53d129d3c6860af647
Matt Tucker <matt.watkinsmedia.org>
2022-01-04 17:37:11 +0000

    Reset Theme pref to Color/White/Black rather than erroneous sgPlayer replacements

--- modules/pref/src/main/Theme.scala
+++ modules/pref/src/main/Theme.scala
@@ -1,14 +1,14 @@
 package lila.pref
 import strategygames.{ GameFamily }
 
-sealed class Theme private[pref] (val name: String, val sgPlayers: Theme.HexSGPlayers, val gameFamily: Int) {
+sealed class Theme private[pref] (val name: String, val colors: Theme.HexColors, val gameFamily: Int) {
 
   override def toString = name
 
   def cssClass = s"${gameFamilyName}-${name}"
 
-  def light = sgPlayers._1
-  def dark  = sgPlayers._2
+  def light = colors._1
+  def dark  = colors._2
   def gameFamilyName = GameFamily(gameFamily).shortName.toLowerCase()
 }
 
@@ -32,22 +32,22 @@ sealed trait ThemeObject {
 
 object Theme extends ThemeObject {
 
-  case class HexSGPlayer(value: String) extends AnyVal with StringValue
-  type HexSGPlayers = (HexSGPlayer, HexSGPlayer)
-  private[pref] val defaultHexSGPlayers = (HexSGPlayer("b0b0b0"), HexSGPlayer("909090"))
+  case class HexColor(value: String) extends AnyVal with StringValue
+  type HexColors = (HexColor, HexColor)
+  private[pref] val defaultHexColors = (HexColor("b0b0b0"), HexColor("909090"))
 
-  val sgPlayers: Map[String, HexSGPlayers] = Map(
-    "blue"   -> (HexSGPlayer("dee3e6") -> HexSGPlayer("8ca2ad")),
-    "brown"  -> (HexSGPlayer("f0d9b5") -> HexSGPlayer("b58863")),
-    "green"  -> (HexSGPlayer("ffffdd") -> HexSGPlayer("86a666")),
-    "purple" -> (HexSGPlayer("9f90b0") -> HexSGPlayer("7d4a8d")),
-    "ic"     -> (HexSGPlayer("ececec") -> HexSGPlayer("c1c18e")),
-    "horsey" -> (HexSGPlayer("f1d9b6") -> HexSGPlayer("8e6547"))
+  val colors: Map[String, HexColors] = Map(
+    "blue"   -> (HexColor("dee3e6") -> HexColor("8ca2ad")),
+    "brown"  -> (HexColor("f0d9b5") -> HexColor("b58863")),
+    "green"  -> (HexColor("ffffdd") -> HexColor("86a666")),
+    "purple" -> (HexColor("9f90b0") -> HexColor("7d4a8d")),
+    "ic"     -> (HexColor("ececec") -> HexColor("c1c18e")),
+    "horsey" -> (HexColor("f1d9b6") -> HexColor("8e6547"))
   )
 
   lazy val default = allByName(0) get "maple" err "Can't find default theme D:"
 
-  val defaults = GameFamily.all.map(gf => new Theme(gf.boardThemeDefault, Theme.sgPlayers.getOrElse(gf.boardThemeDefault, Theme.defaultHexSGPlayers), gf.id))
+  val defaults = GameFamily.all.map(gf => new Theme(gf.boardThemeDefault, Theme.colors.getOrElse(gf.boardThemeDefault, Theme.defaultHexColors), gf.id))
 
   def updateBoardTheme(currentThemes : List[Theme], theme: String, gameFamily: Int) : List[Theme] = {
     val newTheme = apply(theme, gameFamily)
@@ -66,16 +66,16 @@ object Theme extends ThemeObject {
                      }}
   }
 
-  val all: List[Theme] = GameFamily.all.map(gf => gf.boardThemes.map(t => new Theme(t, Theme.sgPlayers.getOrElse(t, Theme.defaultHexSGPlayers), gf.id))).flatten
+  val all: List[Theme] = GameFamily.all.map(gf => gf.boardThemes.map(t => new Theme(t, Theme.colors.getOrElse(t, Theme.defaultHexColors), gf.id))).flatten
  
-  def allOfFamily(gf: GameFamily) : List[Theme] = gf.boardThemes.map(t => new Theme(t, Theme.sgPlayers.getOrElse(t, Theme.defaultHexSGPlayers), gf.id))
+  def allOfFamily(gf: GameFamily) : List[Theme] = gf.boardThemes.map(t => new Theme(t, Theme.colors.getOrElse(t, Theme.defaultHexColors), gf.id))
 
 }
 
 object Theme3d extends ThemeObject {
 
   val all = List(
-    "P2-P1-Aluminium",
+    "Black-White-Aluminium",
     "Brushed-Aluminium",
     "China-Blue",
     "China-Green",
@@ -87,7 +87,7 @@ object Theme3d extends ThemeObject {
     "Green-Glass",
     "Light-Wood",
     "Power-Coated",
-    "Purple-P2",
+    "Purple-Black",
     "Rosewood",
     "Wood-Glass",
     "Marble",
@@ -95,7 +95,7 @@ object Theme3d extends ThemeObject {
     "Jade",
     "Woodi"
   ) map { name =>
-    new Theme(name, Theme.defaultHexSGPlayers, 0)
+    new Theme(name, Theme.defaultHexColors, 0)
   }
 
   lazy val default = allByName(0) get "Woodi" err "Can't find default theme D:"
