69b9dedeead27de8257a3655af00c37f51f1fbe3
James Heppell <jheps@DESKTOP-IOQBCIN.localdomain>
2022-01-26 11:24:53 +0000

    send tv game on lobby to round view instead of tv, when no games in play, as it wont be there

--- app/views/lobby/home.scala
+++ app/views/lobby/home.scala
@@ -152,7 +152,7 @@ object home {
         ),
         featured map { g =>
           div(cls := "lobby__tv")(
-            views.html.game.mini(Pov naturalOrientation g, tv = true)
+            views.html.game.mini(Pov naturalOrientation g, tv = (homepage.counters.rounds > 0))
           )
         },
         puzzle map { p =>
