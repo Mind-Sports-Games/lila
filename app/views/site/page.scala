e6f269cc8b4e6b6eca992ca69718aada100abd3f
James Heppell <jheps@DESKTOP-IOQBCIN.localdomain>
2022-02-01 10:57:32 +0000

    small changes, remove bot link, add changelog, update faq

--- app/views/site/page.scala
+++ app/views/site/page.scala
@@ -218,7 +218,7 @@ $('#asset-version-message').text(playstrategy.info.message);"""
           sep,
           a(activeCls("source"), href := routes.Page.source)(trans.sourceCode()),
           a(activeCls("help"), href := routes.Page.help)(trans.contribute()),
-          //a(activeCls("changelog"), href := routes.Page.menuBookmark("changelog"))("Changelog"),
+          a(activeCls("changelog"), href := routes.Page.menuBookmark("changelog"))("Changelog"),
           a(activeCls("thanks"), href := "/thanks")(trans.thankYou()),
           sep,
           //a(activeCls("webmasters"), href := routes.Main.webmasters)(trans.webmasters()),
