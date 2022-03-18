4cece5c35f0e3cc7f4e061d34752e423bd04c94c
Lakin Wecker <lakin.wecker@gmail.com>
2022-03-17 21:05:31 -0600

    Properly send the right version of fen

--- modules/fishnet/src/main/FishnetRedis.scala
+++ modules/fishnet/src/main/FishnetRedis.scala
@@ -59,7 +59,7 @@ final class FishnetRedis(
       work.level,
       work.clock ?? writeClock,
       work.game.variant.some.filter(_.exotic).??(_.key),
-      work.game.initialFen,
+      work.game.initialFen.??(_.value),
       work.game.moves
     ) mkString ";"
 
