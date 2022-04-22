ebb53771869da348b9480db6f015f907c54eca53
James Heppell <jheps@DESKTOP-IOQBCIN.localdomain>
2022-04-22 17:45:06 +0100

    update build to use local maven repos for changes in oware engine for strategygames

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -16,7 +16,7 @@ object BuildSettings {
     Defaults.coreDefaultSettings ++ Seq(
       version := lilaVersion,
       organization := "org.lichess",
-      resolvers ++= Seq(lilaMaven),
+      resolvers ++= Seq(lilaMaven, wslLocalMaven, dockerLocalMaven), //TODO change to use sys.env as in SG
       scalaVersion := globalScalaVersion,
       scalacOptions ++= compilerOptions,
       // No bloop project for tests
