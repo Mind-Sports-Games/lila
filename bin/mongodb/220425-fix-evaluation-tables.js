bfd60f2fb9c4f55cc8a2aae3862a5ef9526a8a78
Lakin Wecker <lakin.wecker@gmail.com>
2022-04-25 12:09:09 -0600

    Formatting

--- bin/mongodb/220425-fix-evaluation-tables.js
+++ bin/mongodb/220425-fix-evaluation-tables.js
@@ -1,11 +1,11 @@
-  db.player_assessment.find({ "date.$date":  {$exists: true } }).forEach(s => {    
-    db.player_assessment.update(
-      { _id: s._id }, 
-      { 
-        $set: { 
-          date: ISODate(s.date["$date"])
-        },
-      } 
-    );
-  });
+db.player_assessment.find({ "date.$date": { $exists: true } }).forEach(s => {
+  db.player_assessment.update(
+    { _id: s._id },
+    {
+      $set: {
+        date: ISODate(s.date["$date"])
+      },
+    }
+  );
+});
 
