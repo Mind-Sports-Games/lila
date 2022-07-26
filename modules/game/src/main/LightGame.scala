45785076f1551c7d8482716008b06c7f3ec02880
Matt Tucker <matt.watkinsmedia.org>
2022-07-26 18:05:50 +0100

    Add variant icon to player results list

--- modules/game/src/main/LightGame.scala
+++ modules/game/src/main/LightGame.scala
@@ -1,6 +1,7 @@
 package lila.game
 
-import strategygames.{ Player => PlayerIndex, Status }
+import strategygames.{ GameLogic, Player => PlayerIndex, Status }
+import strategygames.variant.Variant
 
 import lila.user.User
 
@@ -8,16 +9,19 @@ case class LightGame(
     id: Game.ID,
     p1Player: Player,
     p2Player: Player,
-    status: Status
+    status: Status,
+    lib: Int,
+    variant_id: Int
 ) {
   def playable                                        = status < Status.Aborted
-  def player(playerIndex: PlayerIndex): Player                    = playerIndex.fold(p1Player, p2Player)
+  def player(playerIndex: PlayerIndex): Player        = playerIndex.fold(p1Player, p2Player)
   def player(playerId: Player.ID): Option[Player]     = players find (_.id == playerId)
   def players                                         = List(p1Player, p2Player)
   def playerByUserId(userId: User.ID): Option[Player] = players.find(_.userId contains userId)
   def winner                                          = players find (_.wins)
-  def wonBy(c: PlayerIndex): Option[Boolean]                = winner.map(_.playerIndex == c)
+  def wonBy(c: PlayerIndex): Option[Boolean]          = winner.map(_.playerIndex == c)
   def finished                                        = status >= Status.Mate
+  def variant                                         = Variant.orDefault(GameLogic(lib), variant_id)
 }
 
 object LightGame {
@@ -26,10 +30,12 @@ object LightGame {
 
   def projection =
     lila.db.dsl.$doc(
-      F.p1Player -> true,
-      F.p2Player -> true,
-      F.playerUids  -> true,
+      F.p1Player          -> true,
+      F.p2Player          -> true,
+      F.playerUids        -> true,
       F.winnerPlayerIndex -> true,
-      F.status      -> true
+      F.status            -> true,
+      F.lib               -> true,
+      F.variant           -> true
     )
 }
