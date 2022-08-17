89f35940676ecd6d4783f1ae43db17a5d7cb6fb7
Matt Tucker <matt.watkinsmedia.org>
2022-08-17 09:57:02 +0100

    Remove logging from pairing system

--- modules/tournament/src/main/arena/PairingSystem.scala
+++ modules/tournament/src/main/arena/PairingSystem.scala
@@ -26,7 +26,7 @@ final private[tournament] class PairingSystem(
     for {
       lastOpponents        <- pairingRepo.lastOpponents(tour.id, users.allIds, Math.min(300, users.size * 4))
       onlyTwoActivePlayers <- (tour.nbPlayers <= 12) ?? playerRepo.countActive(tour.id).dmap(2 ==)
-      data = Data(tour, lastOpponents.pp("lastOpponents"), ranking, onlyTwoActivePlayers.pp("only2AP"))
+      data = Data(tour, lastOpponents, ranking, onlyTwoActivePlayers)
       preps    <- (lastOpponents.hash.isEmpty || users.haveWaitedEnough) ?? evenOrAll(data, users)
       pairings <- prepsToPairings(preps)
     } yield pairings
