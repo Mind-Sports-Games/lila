b01ad550919f54876b68a5e17a75d36d931e13d2
Lakin Wecker <lakin@wecker.ca>
2022-12-23 09:19:59 -0700

    Formatting

--- ui/stratutils/src/promotion.ts
+++ ui/stratutils/src/promotion.ts
@@ -3,32 +3,36 @@ import { Api as ChessgroundApi } from 'chessground/api';
 export const forcedShogiPromotion = (chessground: ChessgroundApi, orig: Key, dest: Key): boolean | undefined => {
   const piece = chessground.state.pieces.get(dest),
     premovePiece = chessground.state.pieces.get(orig);
-  const isP1 = piece && piece.playerIndex == 'p1'
-  const isP2 = piece && piece.playerIndex == 'p2'
+  const isP1 = piece && piece.playerIndex == 'p1';
+  const isP2 = piece && piece.playerIndex == 'p2';
   return (
     (((piece && (piece.role === 'l-piece' || piece.role === 'p-piece') && !premovePiece) ||
       (premovePiece && (premovePiece.role === 'l-piece' || premovePiece.role === 'p-piece'))) &&
       ((dest[1] === '9' && isP1) || (dest[1] == '1' && isP2))) ||
     (((piece && piece.role === 'n-piece' && !premovePiece) || (premovePiece && premovePiece.role === 'n-piece')) &&
-      ((['8', '9'].includes(dest[1]) && isP1) ||
-        (['1', '2'].includes(dest[1]) && isP2)))
+      ((['8', '9'].includes(dest[1]) && isP1) || (['1', '2'].includes(dest[1]) && isP2)))
   );
-}
+};
 
 // forced promotion for shogi pawn in last rank
 // assumes possible promotion is passed through (therefore no checks for drops etc).
 export const forcedMiniShogiPromotion = (chessground: ChessgroundApi, orig: Key, dest: Key): boolean | undefined => {
   const piece = chessground.state.pieces.get(dest),
     premovePiece = chessground.state.pieces.get(orig);
-  const isP1 = piece && piece.playerIndex == 'p1'
-  const isP2 = piece && piece.playerIndex == 'p2'
+  const isP1 = piece && piece.playerIndex == 'p1';
+  const isP2 = piece && piece.playerIndex == 'p2';
   return (
     ((piece && piece.role === 'p-piece' && !premovePiece) || (premovePiece && premovePiece.role === 'p-piece')) &&
     ((dest[1] === '5' && isP1) || (dest[1] == '1' && isP2))
   );
-}
+};
 
-export const possiblePromotion = (chessground: ChessgroundApi, orig: Key, dest: Key, variant: VariantKey): boolean | undefined => {
+export const possiblePromotion = (
+  chessground: ChessgroundApi,
+  orig: Key,
+  dest: Key,
+  variant: VariantKey
+): boolean | undefined => {
   const piece = chessground.state.pieces.get(dest),
     premovePiece = chessground.state.pieces.get(orig);
   const isP1 = piece && piece.playerIndex == 'p1';
@@ -69,4 +73,4 @@ export const possiblePromotion = (chessground: ChessgroundApi, orig: Key, dest: 
           (dest[1] === '1' && piece && piece.playerIndex === 'p2'))
       );
   }
-}
+};
