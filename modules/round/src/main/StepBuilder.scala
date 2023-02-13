ce60602f61e1aedf022dfee8110340d9cc09677f
Matt Tucker <matt.watkinsmedia.org>
2023-02-13 13:08:49 +0000

    Correct debug id

--- modules/round/src/main/StepBuilder.scala
+++ modules/round/src/main/StepBuilder.scala
@@ -17,7 +17,7 @@ object StepBuilder {
       variant: Variant,
       initialFen: FEN
   ): JsArray = {
-    val logMoves = if (id == "Es2nGwAE") pgnMoves.pp("pgnMoves") else pgnMoves
+    val logMoves = if (id == "6A5dmaRK") pgnMoves.pp("pgnMoves") else pgnMoves
     Replay.gameMoveWhileValid(variant.gameLogic, pgnMoves, initialFen, variant) match {
       case (init, games, error) =>
         error foreach logChessError(id)
