72b1c19b361012c2bc2860271626c72f42acfa21
Matt Tucker <matt.watkinsmedia.org>
2023-03-31 15:25:57 +0100

    yarn run prettier

--- bin/mongodb/shield-leaderboard-points.js
+++ bin/mongodb/shield-leaderboard-points.js
@@ -14,7 +14,7 @@ db.tournament2.find({ 'schedule.freq': 'shield', variant: { $exists: false } }).
   db.tournament_leaderboard.updateMany({ t: t._id }, { $set: { k: '0_1' } });
 });
 
-db.tournament2.find({ 'schedule.freq': {$in: ['shield', 'medleyshield'] }}).forEach(t => {
+db.tournament2.find({ 'schedule.freq': { $in: ['shield', 'medleyshield'] } }).forEach(t => {
   db.tournament_leaderboard.updateMany({ t: t._id, r: 1 }, { $set: { mp: 3 } });
   db.tournament_leaderboard.updateMany({ t: t._id, r: 2 }, { $set: { mp: 1 } });
   db.tournament_leaderboard.updateMany({ t: t._id, r: { $gt: 2 } }, { $set: { mp: 0 } });
