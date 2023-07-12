21d4a6b1a72e8f9ce674bba0771df0e3bdfe8931
JamesHeppell <jheppell100@hotmail.com>
2023-07-12 18:13:25 +0100

    fix go setup options and score display

--- ui/analyse/src/util.ts
+++ ui/analyse/src/util.ts
@@ -247,7 +247,7 @@ export function getMancalaScore(fen: string, playerIndex: string): number {
 }
 
 export function getGoScore(fen: string, playerIndex: string): number {
-  return +fen.split(' ')[playerIndex === 'p1' ? 3 : 4];
+  return +fen.split(' ')[playerIndex === 'p1' ? 3 : 4] / 10.0;
 }
 
 const noCevalVariants = [
--- ui/round/src/util.ts
+++ ui/round/src/util.ts
@@ -143,7 +143,7 @@ export function getMancalaScore(fen: string, playerIndex: string): number {
 }
 
 export function getGoScore(fen: string, playerIndex: string): number {
-  return +fen.split(' ')[playerIndex === 'p1' ? 3 : 4];
+  return +fen.split(' ')[playerIndex === 'p1' ? 3 : 4] / 10.0;
 }
 
 export const noChecks: CheckCount = {
