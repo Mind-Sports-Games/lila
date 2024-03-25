46917574182fed746337eb3a0d180d14dda0cdd2
JamesHeppell <jheppell100@hotmail.com>
2024-03-25 10:31:34 +0000

    move trophies to own row

--- app/views/user/show/otherTrophies.scala
+++ app/views/user/show/otherTrophies.scala
@@ -86,32 +86,7 @@ object otherTrophies {
           )(
             img(src := assetUrl(s"images/trophy/${t.kind._id}.png"))
           )
-        },
-      info.trophies.filter(_.kind.klass.has("icon3d")).sorted.map { trophy =>
-        trophy.kind.icon.map { iconChar =>
-          a(
-            awardCls(trophy),
-            href := trophy.kind.url,
-            ariaTitle(trophy.kind.name)
-          )(raw(iconChar))
         }
-      },
-      /*info.isCoach option
-        a(
-          href := routes.Coach.show(info.user.username),
-          cls := "trophy award icon3d coach",
-          ariaTitle(trans.coach.playstrategyCoach.txt())
-        )(":"),*/
-      (info.isStreamer && ctx.noKid) option {
-        val streaming = isStreaming(info.user.id)
-        views.html.streamer.bits.redirectLink(info.user.username, streaming.some)(
-          cls := List(
-            "trophy award icon3d streamer" -> true,
-            "streaming"                    -> streaming
-          ),
-          ariaTitle(if (streaming) "Live now!" else "PlayStrategy Streamer")
-        )("")
-      }
     )
 
   private def awardCls(t: Trophy) = cls := s"trophy award ${t.kind._id} ${~t.kind.klass}"
