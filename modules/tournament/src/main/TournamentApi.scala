adfc5ee665832e49a475fb8160fe49021a18b8bd
Matt Tucker <matt.watkinsmedia.org>
2024-04-09 13:15:52 +0100

    Revert "Add extra logging for tournament DQ reassign medals"
    This reverts commit fb2ca30d463ee3dbf08de4b03977f13c3f3fe0f1.

--- modules/tournament/src/main/TournamentApi.scala
+++ modules/tournament/src/main/TournamentApi.scala
@@ -286,13 +286,13 @@ final class TournamentApi(
   }
 
   private def awardTrophyByRank(tour: Tournament, trophyKind: String, rank: Int, date: DateTime) =
-    playerRepo.bestByTourWithRank(tourId = tour.id, nb = rank.pp("rank"), noDQs = true).flatMap {
-      _.pp("bestByTourWithRank").map {
+    playerRepo.bestByTourWithRank(tourId = tour.id, nb = rank, noDQs = true).flatMap {
+      _.map {
         case rp if rp.rank == rank =>
           trophyApi.award(
             Tournament.tournamentUrl(tour.id),
             rp.player.userId,
-            trophyKind.pp("trophyKind"),
+            trophyKind,
             tour.name.some,
             tour.trophyExpiryDays,
             date
