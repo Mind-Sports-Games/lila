62821e5d10cd882f862143c0a3c1ee033bf1b968
Vincent FROCHOT <vincentfrochot@gmail.com>
2024-06-24 15:55:34 +0200

    SG 139, chessground 2.45, stratops 4.2

--- updateLocalStratops.sh
+++ updateLocalStratops.sh
@@ -1,8 +1,16 @@
-yarn workspace analyse add file:/home/vincent/Desktop/code/stratops
-yarn workspace ceval add file:/home/vincent/Desktop/code/stratops
-yarn workspace dgt add file:/home/vincent/Desktop/code/stratops
-yarn workspace editor add file:/home/vincent/Desktop/code/stratops
-yarn workspace puz add file:/home/vincent/Desktop/code/stratops
-yarn workspace puzzle add file:/home/vincent/Desktop/code/stratops
-yarn workspace racer add file:/home/vincent/Desktop/code/stratops
-yarn workspace storm add file:/home/vincent/Desktop/code/stratops
+# yarn workspace analyse add file:/home/vincent/Desktop/code/stratops
+# yarn workspace ceval add file:/home/vincent/Desktop/code/stratops
+# yarn workspace dgt add file:/home/vincent/Desktop/code/stratops
+# yarn workspace editor add file:/home/vincent/Desktop/code/stratops
+# yarn workspace puz add file:/home/vincent/Desktop/code/stratops
+# yarn workspace puzzle add file:/home/vincent/Desktop/code/stratops
+# yarn workspace racer add file:/home/vincent/Desktop/code/stratops
+# yarn workspace storm add file:/home/vincent/Desktop/code/stratops
+yarn workspace analyse add git+ssh://git@github.com:Mind-Sports-Games/stratops.git#v0.8.1-pstrat4.2
+yarn workspace ceval add git+ssh://git@github.com:Mind-Sports-Games/stratops.git#v0.8.1-pstrat4.2
+yarn workspace dgt add git+ssh://git@github.com:Mind-Sports-Games/stratops.git#v0.8.1-pstrat4.2
+yarn workspace editor git+ssh://git@github.com:Mind-Sports-Games/stratops.git#v0.8.1-pstrat4.2
+yarn workspace puz add git+ssh://git@github.com:Mind-Sports-Games/stratops.git#v0.8.1-pstrat4.2
+yarn workspace puzzle add git+ssh://git@github.com:Mind-Sports-Games/stratops.git#v0.8.1-pstrat4.2
+yarn workspace racer add git+ssh://git@github.com:Mind-Sports-Games/stratops.git#v0.8.1-pstrat4.2
+yarn workspace storm add git+ssh://git@github.com:Mind-Sports-Games/stratops.git#v0.8.1-pstrat4.2
