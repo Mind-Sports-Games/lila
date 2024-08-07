f09cccff7b0a804b435beecc3d0f01d402af3b12
Matt Tucker <matt.watkinsmedia.org>
2024-08-07 14:43:53 +0100

    Reword BRKTHRU variant explanation

--- ui/lobby/src/variant.ts
+++ ui/lobby/src/variant.ts
@@ -15,7 +15,7 @@ type variantConfirmsType = {
   frisian: string;
   frysk: string;
   antidraughts: string;
-  breakthrough: string; // @TODO: rename into BRKTHRU ?
+  breakthrough: string;
   shogi: string;
   minishogi: string;
   xiangqi: string;
@@ -59,7 +59,7 @@ const variantConfirms: variantConfirmsType = {
   frysk: 'This is a Frysk! game!\n\nFrisian draughts starting with 5 pieces each.',
   antidraughts:
     'This is an Antidraughts game!\n\nThe game can be won by losing all your pieces, or running out of moves.',
-  breakthrough: 'This is a Breakthrough game!\n\nThe first player who makes a king wins.',
+  breakthrough: 'This is a BRKTHRU game!\n\nThe first player who makes a king wins.',
   shogi: 'This is a Shogi game!\n\nThe aim of the game is to checkmate the opponent.',
   minishogi: 'This is a Mini Shogi game!\n\nThe aim of the game is to checkmate the opponent.',
   xiangqi: 'This is a Xiangqi game!\n\nThe aim of the game is to checkmate the opponent.',
