d7f04778406544731f35f5b68f6d8f1011277c05
Vincent FROCHOT <vincentfrochot@gmail.com>
2024-09-02 12:07:54 +0200

    chore: trigger asset build

--- ui/site/src/site.ts
+++ ui/site/src/site.ts
@@ -2,7 +2,7 @@ import { boot } from './boot';
 import info from './component/info';
 import windowedPlaystrategy from './site.playstrategy.globals';
 
-// window.playstrategy.load is initialized in layout.scala embedded script tags
+// window.playstrategy.load is initialized in layout.scala embedded script tags.
 
 const l = windowedPlaystrategy();
 l.info = info;
