c878b8d34855fa65d9a49ed9302dc61e899dafc1
Vincent FROCHOT <vincentfrochot@gmail.com>
2025-01-14 14:29:40 +0100

    PLA-1105: add more indexes

--- bin/mongodb/indexes.js
+++ bin/mongodb/indexes.js
@@ -16,6 +16,16 @@ db.game5.createIndex({ ck: 1 }, { sparse: 1, background: 1 });
 db.game5.createIndex({ pl: 1 }, { sparse: true, background: true });
 db.game5.createIndex({ 'pgni.h': 1 }, { sparse: true, background: true });
 
+db.notify.createIndex({ notifies: 1, read: 1, createdAt: -1 });
+db.notify.createIndex({ createdAt: 1 }, { expireAfterSeconds: 2592000 });
+
+db.timeline_entry.createIndex({ users: 1, date: -1 });
+db.timeline_entry.createIndex({ typ: 1, date: -1 });
+
+db.tournament_leaderboard.createIndex({ t: 1 });
+db.tournament_leaderboard.createIndex({ u: 1, d: -1 });
+db.tournament_leaderboard.createIndex({ u: 1, w: 1 });
+
 db.tournament_pairing.createIndex({ tid: 1, d: -1 });
 db.tournament_pairing.createIndex({ tid: 1, u: 1, d: -1 });
 db.tournament_pairing.createIndex({ tid: 1 }, { partialFilterExpression: { s: { $lt: 30 } }, background: 1 });
@@ -23,6 +33,16 @@ db.tournament_pairing.createIndex({ tid: 1 }, { partialFilterExpression: { s: { 
 db.tournament_player.createIndex({ tid: 1, m: -1 });
 db.tournament_player.createIndex({ tid: 1, t: 1, m: -1 }, { partialFilterExpression: { t: { $exists: true } } });
 db.tournament_player.createIndex({ tid: 1, uid: 1 }, { unique: true });
+
+db.tournament2.createIndex({ status: 1 });
+db.tournament2.createIndex({ startsAt: 1 });
+db.tournament2.createIndex({ 'schedule.freq': 1, startsAt: -1 }, { background: true });
+db.tournament2.createIndex({ status: 1, startsAt: 1 }, { partialFilterExpression: { status: 10 }, background: 1 });
+db.tournament2.createIndex({ forTeams: 1, startsAt: -1 }, { partialFilterExpression: { forTeams: { $exists: 1 } } });
+db.tournament2.createIndex(
+  { createdBy: 1, startsAt: -1, status: 1 },
+  { partialFilterExpression: { createdBy: { $exists: true } } },
+);
 
 db.swiss_pairing.createIndex({ s: 1, p: 1, r: 1 });
 db.swiss_pairing.createIndex({ t: 1 }, { partialFilterExpression: { t: true } });
