97766cd8b76a4705edfd656e495e2dc182a9e77c
Vincent FROCHOT <vincentfrochot@gmail.com>
2025-01-23 12:37:27 +0100

    Revert "chore: move code to a more appropriate space"
    This reverts commit 6a459eb20cef2b686204bc40203b0a47308d7589.

--- ui/round/src/util.ts
+++ ui/round/src/util.ts
@@ -286,8 +286,3 @@ export function displayClockPoolUrl(clock: ClockData) {
       ? `${displayClockLimit(clock.initial)}d${clock.delay}`
       : displayClockLimit(clock.initial) + '+' + clock.increment;
 }
-
-export const irrelevantPiecesNamesPerGameFamily = [
-  'togyzkumalak', // "Bastaushi", "Kostaushi"
-  'oware', // "South", "North"
-];
