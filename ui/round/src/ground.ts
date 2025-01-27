b1874b7d05e4e8ac8e95a24b2965075b66039265
Vincent FROCHOT <vincentfrochot@gmail.com>
2025-01-27 16:04:06 +0100

    Revert "feat: no multipointState if target is lt 2"
    This reverts commit baf108b761e84df6cfe7a232d3792ffc2abb68b5.

--- ui/round/src/ground.ts
+++ ui/round/src/ground.ts
@@ -29,12 +29,7 @@ export function makeConfig(ctrl: RoundController): Config {
       ? data.dice
       : stratUtils.readDice(step.fen, data.game.variant.key, data.canEndTurn, ctrl.areDiceDescending),
     doublingCube = data.doublingCube ? data.doublingCube : stratUtils.readDoublingCube(step.fen, data.game.variant.key),
-    cubeActions = data.cubeActions ? data.cubeActions.split(',').map(a => a as cg.CubeAction) : [],
-    multiPointState =
-      data.game.multiPointState && data.game.multiPointState.target && data.game.multiPointState.target > 1
-        ? data.game.multiPointState
-        : null;
-
+    cubeActions = data.cubeActions ? data.cubeActions.split(',').map(a => a as cg.CubeAction) : [];
   return {
     fen: step.fen,
     orientation: boardOrientation(data, ctrl.flip),
@@ -49,7 +44,7 @@ export function makeConfig(ctrl: RoundController): Config {
     cubeActions: cubeActions,
     canUndo: data.canUndo,
     showUndoButton: playing && turnPlayerIndex == data.player.playerIndex && dice.length > 0,
-    multiPointState,
+    multiPointState: data.game.multiPointState,
     addPieceZIndex: ctrl.data.pref.is3d,
     selectOnly: data.selectMode,
     highlight: {
