b41f9692f5521e9d095f7552c80a491e5e1aa70c
Lakin Wecker <lakin@wecker.ca>
2025-02-01 16:36:00 -0700

    fix: upgrade reactive mongo

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -55,7 +55,7 @@ object Dependencies {
   }
 
   object reactivemongo {
-    val version = "1.0.4"
+    val version = "1.0.8"
 
     val driver = "org.reactivemongo" %% "reactivemongo"               % version
     val stream = "org.reactivemongo" %% "reactivemongo-akkastream"    % version
