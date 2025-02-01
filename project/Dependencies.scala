29fe50d5374637b8e2ada0bf62b0132a02d8a969
Lakin Wecker <lakin@wecker.ca>
2025-02-01 07:58:44 -0700

    fix: upgrade specs2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,19 +18,25 @@ object Dependencies {
   val maxmind         = "com.sanoma.cda"             %% "maxmind-geoip2-scala"            % "1.3.1-THIB"
   val prismic         = "io.prismic"                 %% "scala-kit"                       % "1.2.19-THIB213"
   val scrimage        = "com.sksamuel.scrimage"       % "scrimage-core"                   % "4.0.33"
-  val scaffeine       = "com.github.blemale"         %% "scaffeine"                       % "5.2.1"  % "compile"
+  val scaffeine       = "com.github.blemale"         %% "scaffeine"                       % "5.2.1" % "compile"
   val googleOAuth     = "com.google.auth"             % "google-auth-library-oauth2-http" % "0.25.5"
   val scalaUri        = "io.lemonlabs"               %% "scala-uri"                       % "3.2.0"
   val scalatags       = "com.lihaoyi"                %% "scalatags"                       % "0.9.4"
   val lettuce         = "io.lettuce"                  % "lettuce-core"                    % "6.1.2.RELEASE"
   val epoll           = "io.netty"                    % "netty-transport-native-epoll"    % "4.1.58.Final" classifier "linux-x86_64"
-  val autoconfig      = "io.methvin.play"            %% "autoconfig-macros"               % "0.3.2"  % "provided"
-  val scalatest       = "org.scalatest"              %% "scalatest"                       % "3.1.0"  % Test
+  val autoconfig      = "io.methvin.play"            %% "autoconfig-macros"               % "0.3.2" % "provided"
+  val scalatest       = "org.scalatest"              %% "scalatest"                       % "3.1.0" % Test
   val uaparser        = "org.uaparser"               %% "uap-scala"                       % "0.13.0"
-  val specs2          = "org.specs2"                 %% "specs2-core"                     % "4.11.0" % Test
   val apacheText      = "org.apache.commons"          % "commons-text"                    % "1.12.0"
   val bloomFilter     = "com.github.alexandrnikitin" %% "bloom-filter"                    % "0.13.1"
   val jacksonDatabind = "com.fasterxml.jackson.core"  % "jackson-databind"                % "2.10.0"
+
+  object specs2 {
+    val version = "4.18.0"
+    val core    = "org.specs2" %% "specs2-core" % version % Test
+    val cats    = "org.specs2" %% "specs2-cats" % version % Test
+    val bundle  = Seq(core, cats)
+  }
 
   object flexmark {
     val version = "0.50.50"
@@ -59,15 +65,15 @@ object Dependencies {
   }
 
   object play {
-    val version = "2.8.16-lila_1.16"
+    val version = "2.8.16-lila_1.17"
     val api     = "com.typesafe.play" %% "play"         % version
-    val json    = "com.typesafe.play" %% "play-json"    % "2.9.2"
+    val json    = "com.typesafe.play" %% "play-json"    % "2.9.3"
     val logback = "com.typesafe.play" %% "play-logback" % "1.2.13"
     val mailer  = "com.typesafe.play" %% "play-mailer"  % "8.0.1"
   }
 
   object playWs {
-    val version = "2.1.2"
+    val version = "2.2.0-M1"
     val ahc     = "com.typesafe.play" %% "play-ahc-ws-standalone"  % version
     val json    = "com.typesafe.play" %% "play-ws-standalone-json" % version
     val bundle  = Seq(ahc, json)
