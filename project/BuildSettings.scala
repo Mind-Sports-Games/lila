78ce16c75bc230ee9282361cd0d3b6c022facdff
Lakin Wecker <lakin@wecker.ca>
2025-02-01 07:09:24 -0700

    fix: Upgrade to scala 2.13.16

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -7,7 +7,7 @@ object BuildSettings {
   import Dependencies._
 
   val lilaVersion        = "3.2"
-  val globalScalaVersion = "2.13.10"
+  val globalScalaVersion = "2.13.16"
 
   val useEpoll = sys.props.get("epoll").fold(false)(_.toBoolean)
   if (useEpoll) println("--- epoll build ---")
