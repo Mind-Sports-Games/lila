06966d2eedc783db897f742e5b04f801e373040b
Vincent FROCHOT <vincentfrochot@gmail.com>
2026-02-10 17:28:05 +0100

    style: pnpm format

--- ui/common/src/dameoActivePiece.ts
+++ ui/common/src/dameoActivePiece.ts
@@ -2,11 +2,11 @@ import { read as dameoFenRead } from 'chessground/variants/dameo/fen';
 import type { Key as CGKey, Pieces as CGPieces } from 'chessground/types';
 
 export function dameoActivePiece(fen: string): CGKey | undefined {
-    const pieces: CGPieces = dameoFenRead(fen);
-    for (const [key, piece] of pieces) {
-      if (['a-piece', 'b-piece'].includes(piece.role)) {
-        return key;
-      }
+  const pieces: CGPieces = dameoFenRead(fen);
+  for (const [key, piece] of pieces) {
+    if (['a-piece', 'b-piece'].includes(piece.role)) {
+      return key;
     }
-    return undefined;
+  }
+  return undefined;
 }
