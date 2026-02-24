3955ac75f1859ccb7fefe3fc1c2911f366893a8a
JamesHeppell <jheppell100@hotmail.com>
2026-02-24 11:58:29 +0000

    add query for users and puzzle stats

---
+++ bin/mongodb/variant-users-count.js
@@ -0,0 +1,60 @@
+use('lichess');
+
+const variants = [
+  'standard',
+  'chess960',
+  'kingOfTheHill',
+  'threeCheck',
+  'fiveCheck',
+  'antichess',
+  'atomic',
+  'horde',
+  'racingKings',
+  'crazyhouse',
+  'noCastling',
+  'monster',
+  'linesOfAction',
+  'scrambledEggs',
+  'frisian',
+  'frysk',
+  'international',
+  'antidraughts',
+  'breakthrough',
+  'russian',
+  'brazilian',
+  'pool',
+  'portuguese',
+  'english',
+  'dameo',
+  'shogi',
+  'xiangqi',
+  'minishogi',
+  'minixiangqi',
+  'flipello',
+  'flipello10',
+  'antiflipello',
+  'octagonflipello',
+  'amazons',
+  'breakthroughtroyka',
+  'minibreakthroughtroyka',
+  'oware',
+  'togyzkumalak',
+  'bestemshe',
+  'go9x9',
+  'go13x13',
+  'go19x19',
+  'backgammon',
+  'hyper',
+  'nackgammon',
+  'abalone',
+];
+
+const facet = Object.fromEntries(
+  variants.map(v => [v, [{ $match: { [`perfs.${v}.nb`]: { $gt: 0 } } }, { $count: 'count' }]]),
+);
+
+const project = Object.fromEntries(variants.map(v => [v, { $arrayElemAt: [`$${v}.count`, 0] }]));
+
+const result = db.user4.aggregate([{ $facet: facet }, { $project: project }]).toArray()[0];
+
+variants.forEach(v => print(`${v}\t${result[v] ?? 0}`));
