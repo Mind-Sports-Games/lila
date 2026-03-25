68164e7b175ed504c702e170d351a6194793930d
Vincent FROCHOT <vincentfrochot@gmail.com>
2026-03-25 23:31:10 +0100

    fix: lobby clocks and leaderboard GA key

--- app/views/user/list.scala
+++ app/views/user/list.scala
@@ -99,7 +99,7 @@ object list {
               userTopPerf(leaderboards.hyper, PerfType.orDefault("hyper")),
               userTopPerf(leaderboards.nackgammon, PerfType.orDefault("nackgammon")),
               userTopPerf(leaderboards.abalone, PerfType.orDefault("abalone")),
-              userTopPerf(leaderboards.abalone, PerfType.orDefault("grandabalone"))
+              userTopPerf(leaderboards.grandabalone, PerfType.orDefault("grandabalone"))
             )
           )
         )
