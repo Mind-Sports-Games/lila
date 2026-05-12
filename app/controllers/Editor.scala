b63fca93b47739b83a5ec000373e5021457dba55
Vincent FROCHOT <vincentfrochot@gmail.com>
2026-05-12 17:14:47 +0200

    fix: enable startingPositions for Abalone in board editor

--- app/controllers/Editor.scala
+++ app/controllers/Editor.scala
@@ -3,6 +3,9 @@ package controllers
 import strategygames.variant.Variant
 import strategygames.format.{ FEN, Forsyth }
 import strategygames.Situation
+import strategygames.abalone.opening.EcopeningDB
+import strategygames.abalone.{ Replay => AbaloneReplay }
+import strategygames.abalone.format.{ Forsyth => AbaloneForsyth, Uci => AbaloneUci }
 import play.api.libs.json.*
 import views.*
 import strategygames.chess.format.Forsyth as ChessForsyth
@@ -17,12 +20,48 @@ final class Editor(env: Env) extends LilaController(env) {
       Json.obj("eco" -> p.eco, "name" -> p.name, "fen" -> p.fen)
     })
 
+  private def abalonePositionsData(variantGrouping: String): JsArray = {
+    val startingPositions = strategygames.abalone.StartingPosition.forVariant(variantGrouping).map { p =>
+      Json.obj("eco" -> p.eco, "name" -> p.name, "fen" -> p.fen.value)
+    }
+    val abaloneVariant = variantGrouping match {
+      case "grandabalone" => strategygames.abalone.variant.GrandAbalone
+      case _              => strategygames.abalone.variant.Abalone
+    }
+    val openings = EcopeningDB.all
+      .filter(_.variantGrouping == variantGrouping)
+      .sortBy(_.eco)
+      .map { p =>
+        val ucis = p.moves.split("\\s+").toList.flatMap(AbaloneUci.Move(_))
+        val fen = AbaloneReplay(ucis, None, abaloneVariant)
+          .map(r => AbaloneForsyth.>>(r.state).value)
+          .getOrElse(s"${p.fen} 0 0 b 0 1")
+        Json.obj(
+          "eco"  -> p.eco,
+          "name" -> s"${p.family}: ${p.name}",
+          "fen"  -> fen
+        )
+      }
+    JsArray(startingPositions ++ openings)
+  }
+
+  private lazy val abalonePositionsDataCached: JsArray      = abalonePositionsData("abalone")
+  private lazy val grandAbalonePositionsDataCached: JsArray = abalonePositionsData("grandabalone")
+
   private lazy val positionsByVariantJson = lila.common.String.html.safeJsonValue {
-    Json.obj("chess" -> chessPositionsData)
+    Json.obj(
+      "chess"        -> chessPositionsData,
+      "abalone"      -> abalonePositionsDataCached,
+      "grandabalone" -> grandAbalonePositionsDataCached
+    )
   }
 
   private def positionsJsonForVariant(variantKey: String) = lila.common.String.html.safeJsonValue {
-    chessPositionsData
+    variantKey match {
+      case "abalone"      => abalonePositionsDataCached
+      case "grandabalone" => grandAbalonePositionsDataCached
+      case _              => chessPositionsData
+    }
   }
 
   def index = load("")
